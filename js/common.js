efdd4f328e7f362bd587c8b80883878b5ea2f6ad
johyemin <49671771+johyemin@users.noreply.github.com>
2024-01-02 12:35:42 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -6,5 +6,24 @@ $(function(){
   $(".close").click(function () {
     $(".submenu").stop().animate({ right: '-100%' }, 1000)
   })
+
+  $(".slide").slick({
+    slidesToShow: 1,
+    autoplay: true,
+    infinite: false,
+    responsive: [
+        // 배열 안에 객체를 넣는다
+        {
+            // 반응형 잡는 법
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+
+
+    ]
+})
+
 ///////
 });
